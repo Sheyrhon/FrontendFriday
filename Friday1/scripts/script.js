a68afd267aa4d071ace0c7d10e0cb9ec0c8c81d7
Sheyrhon <chisheyrhon96@gmail.com>
2024-01-24 10:10:30 +0100

    took off comments and formatted code

--- Friday1/scripts/script.js
+++ Friday1/scripts/script.js
@@ -1,48 +1,42 @@
-let userInput = '';
-let problem = '';
-//function is called when a number or decimal point is clicked, after 
-//appending it updates the result by setting it to the result input fiels
-const appendValue =(value)=>{
-    userInput += value;
-    document.getElementById('result').value= userInput;
-    document.getElementById("problem").textContent = problem;
-};
-//this appends the operator when the operator button id clicked
-const appendOperator = (operator)=>{
-userInput +=operator;
-document.getElementById('result').value = userInput;
+let userInput = "";
+let problem = "";
 
+const appendValue = (value) => {
+  userInput += value;
+  document.getElementById("result").value = userInput;
+  document.getElementById("problem").textContent = problem;
 };
-// this function is called when the equal sign is clicked
-// incase of error it catches and displays error
-const calculate =()=>{
-    try{
-        const result = eval(userInput);
-        document.getElementById('result').value= result;
-        userInput='';
-    } catch(error){
-        document.getElementById('result').value ='Error';
-        userInput ='';
-    }
-}
-// this called when reset button is clicked, clears the expression screen adn sets
-// it to an empty string
-const clearResult=()=>{
-    userInput='';
-    document.getElementById('result').value = '';
-}
-// delete funtion
-const Del=()=> {
-    userInput='';
-     document.getElementById('userInput');
-    
-    
-    userInput.value = userInput.value.slice(0, -1);
-    
-    try {
-      const result = eval(userInput.value);
-      result.value = result;
-    } catch (error) {
-      result.value = 'Error';
-    }
+
+const appendOperator = (operator) => {
+  userInput += operator;
+  document.getElementById("result").value = userInput;
+};
+
+const calculate = () => {
+  try {
+    const result = eval(userInput);
+    document.getElementById("result").value = result;
+    userInput = "";
+  } catch (error) {
+    document.getElementById("result").value = "Error";
+    userInput = "";
   }
+};
+
+const clearResult = () => {
+  userInput = "";
+  document.getElementById("result").value = "";
+};
+
+const Del = () => {
+  userInput = "";
+  document.getElementById("userInput");
+  userInput.value = userInput.value.slice(0, -1);
+
+  try {
+    const result = eval(userInput.value);
+    result.value = result;
+  } catch (error) {
+    result.value = "Error";
+  }
+};
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # FrontendFriday
+link to figma design: https://www.figma.com/file/RUha7vCUaRHagdlDQFJPtg/Calculator?node-id=3%3A190&mode=dev
+
+Deployed site: https://steady-cendol-11ed7f.netlify.app/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
